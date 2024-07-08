10ae9749dfa0b969254dd94b115f0ba39153a8c9
손찬형 <sonchanhyeong710@gmail.com>
2024-07-09 08:48:01 +0900

    MainScreen 기본설정

--- app/src/main/java/com/sonchan/dobakapppractice/presentation/MainScreen.kt
+++ app/src/main/java/com/sonchan/dobakapppractice/presentation/MainScreen.kt
@@ -4,9 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import com.sonchan.dobakapppractice.ui.theme.DobakAppPracticeTheme
 
-class MainScreen : ComponentActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,4 +24,19 @@ class MainScreen : ComponentActivity() {
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun MainScreenPreview(){
+    MainScreen()
+}
+
+@Composable
+fun MainScreen(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFFFFFFFF))
+    )
 }
