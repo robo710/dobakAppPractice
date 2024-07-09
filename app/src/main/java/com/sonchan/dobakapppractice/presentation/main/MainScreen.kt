d3806b2d9b7391feb55940083b21cfb091f6ab1a
손찬형 <sonchanhyeong710@gmail.com>
2024-07-09 11:01:52 +0900

    MoneyCount 위치 변경

--- app/src/main/java/com/sonchan/dobakapppractice/presentation/main/MainScreen.kt
+++ app/src/main/java/com/sonchan/dobakapppractice/presentation/main/MainScreen.kt
@@ -1,6 +1,5 @@
 package com.sonchan.dobakapppractice.presentation.main
 
-import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
