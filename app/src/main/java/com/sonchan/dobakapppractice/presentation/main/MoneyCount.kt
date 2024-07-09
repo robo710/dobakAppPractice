996200dbe7a701e362ea8ebe482ee7133b6f26ab
손찬형 <sonchanhyeong710@gmail.com>
2024-07-09 10:21:44 +0900

    formatter 추가

--- app/src/main/java/com/sonchan/dobakapppractice/presentation/main/MoneyCount.kt
+++ app/src/main/java/com/sonchan/dobakapppractice/presentation/main/MoneyCount.kt
@@ -1,8 +1,34 @@
 package com.sonchan.dobakapppractice.presentation.main
 
+import android.icu.text.DecimalFormat
+import android.icu.text.NumberFormat
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun MoneyCountPreview(){
+    MoneyCount(money = 1000000000000000000)
+}
 
 @Composable
-fun MoneyCount() {
+fun MoneyCount(money: Long, modifier: Modifier = Modifier) {
+    val formatter: NumberFormat = DecimalFormat("#,###")
+    val formattedMoney = formatter.format(money)
 
+    Card(
+        modifier = modifier
+    ) {
+        Text(
+            text = ("잔여 돈 : $formattedMoney"),
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.padding(8.dp)
+        )
+    }
 }
