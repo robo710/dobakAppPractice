57c47f1f65421d9edb7adddd880245e8e5b9ce2e
손찬형 <sonchanhyeong710@gmail.com>
2024-07-09 09:53:24 +0900

    OutlinedTextField 추가

--- app/src/main/java/com/sonchan/dobakapppractice/presentation/main/MainScreen.kt
+++ app/src/main/java/com/sonchan/dobakapppractice/presentation/main/MainScreen.kt
@@ -9,14 +9,22 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
 import com.sonchan.dobakapppractice.ui.theme.DobakAppPracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,7 +47,9 @@ fun MainScreenPreview(){
 
 @Composable
 fun MainScreen(){
-
+    var money by remember {
+        mutableStateOf("")
+    }
 
     Column(
         modifier = Modifier
@@ -49,7 +59,13 @@ fun MainScreen(){
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        OutlinedTextField(value = money,
+            onValueChange = { money = it },
+            textStyle = TextStyle(color = Color(0xFF000000)),
+            label = { Text("돈 입력") },
+            placeholder = { Text("EX)1,000,000") },
+            modifier = Modifier.width(350.dp),
+            singleLine = true)
         Button(
             onClick = { /*TODO*/ }
         ) {
