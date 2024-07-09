1cda8f198a55c8c69215c56ff96c2ef8cf115f90
손찬형 <sonchanhyeong710@gmail.com>
2024-07-09 09:04:08 +0900

    button 추가

--- app/src/main/java/com/sonchan/dobakapppractice/presentation/main/MainScreen.kt
+++ app/src/main/java/com/sonchan/dobakapppractice/presentation/main/MainScreen.kt
@@ -5,12 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sonchan.dobakapppractice.ui.theme.DobakAppPracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,9 +39,21 @@ fun MainScreenPreview(){
 
 @Composable
 fun MainScreen(){
-    Box(
+
+
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .background(color = Color(0xFFFFFFFF))
-    )
+            .padding(10.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Button(
+            onClick = { /*TODO*/ }
+        ) {
+            Text(text = "확인")
+        }
+    }
 }
