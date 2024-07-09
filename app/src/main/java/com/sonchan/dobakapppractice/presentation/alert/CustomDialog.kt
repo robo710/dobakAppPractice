cfecd275520f2027a17a85c618d7c1146df8b0a2
손찬형 <sonchanhyeong710@gmail.com>
2024-07-09 17:50:49 +0900

    CustomDialog 만듬

--- app/src/main/java/com/sonchan/dobakapppractice/presentation/alert/CustomDialog.kt
+++ app/src/main/java/com/sonchan/dobakapppractice/presentation/alert/CustomDialog.kt
@@ -1,2 +1,140 @@
 package com.sonchan.dobakapppractice.presentation.alert
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+
+@Preview
+@Composable
+fun CustomAlertDialogPreview(){
+    CustomAlertDialog(title = "돈이 없습니다.", description = "돈을 더 모으세요.",
+        onClickConfirm = { /*TODO*/ })
+}
+
+@Composable
+fun CustomAlertDialog(
+    title: String,
+    description: String,
+    onClickConfirm: () -> Unit
+) {
+    Dialog(
+        onDismissRequest = { onClickConfirm() },
+        properties = DialogProperties(
+            dismissOnBackPress = true,
+            dismissOnClickOutside = true,
+        )
+    ) {
+        Card(
+            shape = RoundedCornerShape(8.dp), // Card의 모든 꼭지점에 8.dp의 둥근 모서리 적용
+        )
+        {
+            Column(
+                modifier = Modifier
+                    .width(200.dp)
+                    .wrapContentHeight()
+                    .background(
+                        color = Color.White,
+                    ),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Text(
+                    text = title,
+                    textAlign = TextAlign.Center,
+                    style = TextStyle(
+                        color = Color.Black,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                Text(
+                    text = description,
+                    textAlign = TextAlign.Center,
+                    style = TextStyle(
+                        color = Color.LightGray,
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Normal
+                    )
+                )
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(1.dp)
+                        .background(color = Color.LightGray)
+                )
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(IntrinsicSize.Min) // Row의 높이를 내부 컴포넌트에 맞춤
+                ) {
+                    Button(
+                        onClick = { onClickConfirm() },
+                        shape = RectangleShape,
+                        modifier = Modifier
+                            .weight(1f)
+                            .fillMaxHeight(),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.White, // 버튼 배경색상
+                            contentColor = Color.Black, // 버튼 텍스트 색상
+                            disabledContainerColor = Color.Gray, // 버튼 비활성화 배경 색상
+                            disabledContentColor = Color.White, // 버튼 비활성화 텍스트 색상
+                        ),
+
+                        ) {
+                        Text(
+                            text = "확인",
+                            textAlign = TextAlign.Center,
+                            style = TextStyle(
+                                fontSize = 14.sp,
+                                fontWeight = FontWeight.Normal
+                            )
+                        )
+                    }
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .width(1.dp)
+                            .background(color = Color.LightGray)
+                    )
+
+
+                }
+            }
+        }
+    }
+}
