e245747ae5792e4136947c1cb50429b42a80f0b3
손찬형 <sonchanhyeong710@gmail.com>
2024-07-11 08:40:21 +0900

    sealed class -> enum class

--- app/src/main/java/com/sonchan/dobakapppractice/presentation/nav/BottomNavItem.kt
+++ app/src/main/java/com/sonchan/dobakapppractice/presentation/nav/BottomNavItem.kt
@@ -2,10 +2,8 @@ package com.sonchan.dobakapppractice.presentation.nav
 
 import com.sonchan.dobakapppractice.R
 
-sealed class BottomNavItem(
-    val title: Int, val icon: Int, val screenRoute: String
-) {
-    object Mine : BottomNavItem(R.string.text_mine, R.drawable.mine, MINE)
-    object Main : BottomNavItem(R.string.text_main, R.drawable.main, MAIN)
-    object Rank : BottomNavItem(R.string.text_rank, R.drawable.rank, RANK)
+enum class BottomNavItem(val title: Int, val icon: Int, val screenRoute: String) {
+    Mine(R.string.text_mine, R.drawable.mine, MINE),
+    Main(R.string.text_main, R.drawable.main, MAIN),
+    Rank(R.string.text_rank, R.drawable.rank, RANK)
 }
