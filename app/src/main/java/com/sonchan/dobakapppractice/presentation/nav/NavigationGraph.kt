025d435269fbfbab298b3240af088248f28a6a8d
손찬형 <sonchanhyeong710@gmail.com>
2024-07-10 13:47:51 +0900

    NavigationGraph 변경

--- app/src/main/java/com/sonchan/dobakapppractice/presentation/nav/NavigationGraph.kt
+++ app/src/main/java/com/sonchan/dobakapppractice/presentation/nav/NavigationGraph.kt
@@ -7,16 +7,17 @@ import androidx.navigation.compose.composable
 import com.sonchan.dobakapppractice.presentation.main.MainScreen
 import com.sonchan.dobakapppractice.presentation.main.MainViewModel
 import com.sonchan.dobakapppractice.presentation.mine.MineScreen
+import com.sonchan.dobakapppractice.presentation.mine.MineViewModel
 import com.sonchan.dobakapppractice.presentation.rank.RankScreen
 
 @Composable
 fun NavigationGraph(navController: NavHostController) {
     NavHost(navController = navController, startDestination = BottomNavItem.Main.screenRoute) {
-        composable(BottomNavItem.Main.screenRoute) {
-            MainScreen(viewModel = MainViewModel())
-        }
+//        composable(BottomNavItem.Main.screenRoute) {
+//            MainScreen(viewModel = MainViewModel(null))
+//        }
         composable(BottomNavItem.Mine.screenRoute) {
-            MineScreen()
+            MineScreen(viewModel = MineViewModel())
         }
         composable(BottomNavItem.Rank.screenRoute) {
             RankScreen()
