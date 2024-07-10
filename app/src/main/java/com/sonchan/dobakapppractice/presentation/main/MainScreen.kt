7f650a58505cfd09d18dd50a9255d710a59e4f6c
손찬형 <sonchanhyeong710@gmail.com>
2024-07-10 15:39:02 +0900

    MainScreen 수정

--- app/src/main/java/com/sonchan/dobakapppractice/presentation/main/MainScreen.kt
+++ app/src/main/java/com/sonchan/dobakapppractice/presentation/main/MainScreen.kt
@@ -1,13 +1,9 @@
 package com.sonchan.dobakapppractice.presentation.main
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.IntentSenderRequest
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -23,7 +19,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -35,118 +30,24 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
-import com.google.android.gms.auth.api.identity.Identity
 import com.google.firebase.FirebaseApp
 import com.sonchan.dobakapppractice.presentation.alert.LackAlert
-import com.sonchan.dobakapppractice.presentation.login.GoogleAuthUiClient
-import com.sonchan.dobakapppractice.presentation.login.LoginScreen
-import com.sonchan.dobakapppractice.presentation.login.LoginViewModel
-import com.sonchan.dobakapppractice.presentation.profile.ProfileScreen
+import com.sonchan.dobakapppractice.presentation.login.GoogleAuthUiClientProvider
+import com.sonchan.dobakapppractice.presentation.nav.setupNavGraph
 import com.sonchan.dobakapppractice.ui.theme.DobakAppPracticeTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-
-    private val googleAuthUiClient by lazy {
-        GoogleAuthUiClient(
-            context = applicationContext,
-            oneTapClient = Identity.getSignInClient(applicationContext)
-        )
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         FirebaseApp.initializeApp(this) // Firebase 초기화
+        GoogleAuthUiClientProvider.initialize(applicationContext) // GoogleAuthUiClient 초기화
         setContent {
             DobakAppPracticeTheme {
                 val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = "login"){
-                    composable("login"){
-                        val viewModel = viewModel<LoginViewModel>()
-                        val state by viewModel.state.collectAsStateWithLifecycle()
-
-                        LaunchedEffect(key1 = Unit) {
-                            if(googleAuthUiClient.getSignedInUser() != null){
-                                navController.navigate("profile")
-                            }
-                        }
-
-                        val launcher = rememberLauncherForActivityResult(
-                            contract = ActivityResultContracts.StartIntentSenderForResult(),
-                            onResult = { result ->
-                                if (result.resultCode == RESULT_OK){
-                                    lifecycleScope.launch {
-                                        val signInResult = googleAuthUiClient.signInWithIntent(
-                                            intent = result.data ?: return@launch
-                                        )
-                                        viewModel.onSignInResult(signInResult)
-                                    }
-                                }
-                            }
-                        )
-                        
-                        LaunchedEffect(key1 = state.isSignInSuccessful) {
-                            if(state.isSignInSuccessful){
-                                Toast.makeText(
-                                    applicationContext,
-                                    "성공",
-                                    Toast.LENGTH_LONG
-                                ).show()
-
-                                navController.navigate("main")
-                                viewModel.resetState()
-                            }
-                        }
-
-                        LoginScreen(
-                            state = state,
-                            onSignInClick = {
-                                lifecycleScope.launch {
-                                    val signInIntentSender = googleAuthUiClient.signIn()
-                                    launcher.launch(
-                                        IntentSenderRequest.Builder(
-                                            signInIntentSender ?: return@launch
-                                        ).build()
-                                    )
-                                }
-                            }
-                        )
-                    }
-                    composable("profile"){
-                        ProfileScreen(
-                            userData = googleAuthUiClient.getSignedInUser(),
-                            onSignOut = {
-                                lifecycleScope.launch {
-                                    googleAuthUiClient.signOut()
-                                    Toast.makeText(
-                                        applicationContext,
-                                        "로그아웃됨",
-                                        Toast.LENGTH_LONG
-                                    ).show()
-
-                                    navController.navigate("login")
-                                }
-                            }
-                        )
-                    }
-                    composable("main") {
-                        MainScreen(
-                            viewModel = MainViewModel
-                                (userData = googleAuthUiClient.getSignedInUser()),
-                            onProfileClick = {
-                                navController.navigate("profile")
-                            }
-                        )
-                    }
-                }
+                setupNavGraph(navController = navController)
             }
         }
     }
--- app/src/main/java/com/sonchan/dobakapppractice/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/sonchan/dobakapppractice/presentation/main/MainViewModel.kt
@@ -5,8 +5,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.sonchan.dobakapppractice.data.UserData
 
-class MainViewModel : ViewModel() {
+class MainViewModel(val userData: UserData?) : ViewModel() {
+
     private var _leftMoney = MutableLiveData<Long>()
     private var _showLackAlert = MutableLiveData<Boolean>()
 
@@ -21,25 +23,27 @@ class MainViewModel : ViewModel() {
         _showLackAlert.value = false
     }
 
-    fun updateValue(input: Long) {
+    fun dobakValue(input: Long) {
         val db = Firebase.firestore
         if (_leftMoney.value!! < input) {
             _showLackAlert.value = true  // LackAlert를 표시하기 위한 상태 변경
         } else {
-            _leftMoney.value = (_leftMoney.value ?: 0) - input
+            if(userData?.username != null) {
+                _leftMoney.value = (_leftMoney.value ?: 0) - input
 
-            val data = hashMapOf(
-                "money" to _leftMoney.value
-            )
+                val data = hashMapOf(
+                    "money" to _leftMoney.value
+                )
 
-            db.collection("messages")
-                .add(data)
-                .addOnSuccessListener { documentReference ->
-                    println("DocumentSnapshot added with ID: ${documentReference.id}")
-                }
-                .addOnFailureListener { e ->
-                    println("Error adding document $e")
-                }
+                db.collection("user").document(userData.userId)
+                    .set(data)
+                    .addOnSuccessListener {
+                        println("DocumentSnapshot successfully written!")
+                    }
+                    .addOnFailureListener { e ->
+                        println("Error writing document $e")
+                    }
+            }
         }
     }
 
