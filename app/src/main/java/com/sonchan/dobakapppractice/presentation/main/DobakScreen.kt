70232b8450fc3075a962d4de6fdd52fa16380b4d
손찬형 <sonchanhyeong710@gmail.com>
2024-07-11 09:46:44 +0900

    MainScreen -> DobakScreen

---
+++ app/src/main/java/com/sonchan/dobakapppractice/presentation/main/DobakScreen.kt
@@ -0,0 +1,101 @@
+package com.sonchan.dobakapppractice.presentation.main
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
+import com.sonchan.dobakapppractice.presentation.alert.LackAlert
+
+@Composable
+fun DobakScreen(
+    onProfileClick: () -> Unit,
+    viewModel: DobakViewModel
+) {
+    val navController = rememberNavController()
+    var money by remember { mutableStateOf("") }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFFFFFFFF))
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp),
+            contentAlignment = Alignment.TopStart
+        ) {
+            IconButton(onClick = onProfileClick) {
+                if (viewModel.userData?.profilePictureUrl != null) {
+                    AsyncImage(
+                        model = viewModel.userData.profilePictureUrl,
+                        contentDescription = "Profile picture",
+                        modifier = Modifier
+                            .size(150.dp)
+                            .clip(CircleShape),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+            }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(
+                value = money,
+                onValueChange = { money = it },
+                textStyle = TextStyle(color = Color(0xFF000000)),
+                label = { Text("돈 입력") },
+                placeholder = { Text("EX)1,000,000") },
+                modifier = Modifier.width(350.dp),
+                singleLine = true
+            )
+            Button(
+                onClick = {
+                    viewModel.dobakValue(
+                        money.toLong()
+                    )
+                }
+            ) {
+                Text(text = "확인")
+            }
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp),
+            contentAlignment = Alignment.TopEnd
+        ) {
+            MoneyCount(money = viewModel.leftMoney)
+        }
+        LackAlert(viewModel = viewModel)
+    }
+}
