e27fe7a79ef0734515eb83fdc2a73ead47be05d6
손찬형 <sonchanhyeong710@gmail.com>
2024-07-11 20:20:25 +0900

    MoneyCount 추가

--- app/src/main/java/com/sonchan/dobakapppractice/presentation/mine/MineScreen.kt
+++ app/src/main/java/com/sonchan/dobakapppractice/presentation/mine/MineScreen.kt
@@ -3,36 +3,43 @@ package com.sonchan.dobakapppractice.presentation.mine
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.rememberNavController
-
-@Preview
-@Composable
-fun MineScreenPreview(){
-    MineScreen(viewModel = MineViewModel())
-}
+import androidx.compose.ui.unit.dp
+import com.sonchan.dobakapppractice.presentation.dobak.DobakViewModel
+import com.sonchan.dobakapppractice.presentation.dobak.MoneyCount
 
 @Composable
-fun MineScreen(viewModel: MineViewModel){
-    val navController = rememberNavController()
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = Color(0xFFFFFFFF)),
-        Alignment.Center
-    ){
-        Button(
-            onClick = {
-                /*TODO*/
-            }
+fun MineScreen(viewModel: DobakViewModel){
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(color = Color(0xFFFFFFFF))
         ) {
-            Text(text = "확인")
+        Box(
+            modifier = Modifier,
+            Alignment.Center
+        ) {
+            Button(
+                onClick = {
+                    viewModel.addMoney()
+                }
+            ) {
+                Text(text = "확인")
+            }
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp),
+            contentAlignment = Alignment.TopEnd
+        ) {
+            MoneyCount(money = viewModel.leftMoney)
         }
     }
 }
