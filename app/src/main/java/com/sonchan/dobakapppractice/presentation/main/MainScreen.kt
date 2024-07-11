d8b7adcb665951a1768a65c39d3330aad1415bbb
손찬형 <sonchanhyeong710@gmail.com>
2024-07-11 12:06:04 +0900

    MainScreen 수정

--- app/src/main/java/com/sonchan/dobakapppractice/presentation/main/MainScreen.kt
+++ app/src/main/java/com/sonchan/dobakapppractice/presentation/main/MainScreen.kt
@@ -4,41 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import coil.compose.AsyncImage
 import com.google.firebase.FirebaseApp
-import com.sonchan.dobakapppractice.presentation.alert.LackAlert
 import com.sonchan.dobakapppractice.presentation.login.GoogleAuthUiClientProvider
+import com.sonchan.dobakapppractice.presentation.nav.BottomNavItem
 import com.sonchan.dobakapppractice.presentation.nav.BottomNavigation
 import com.sonchan.dobakapppractice.presentation.nav.NavigationGraph
-import com.sonchan.dobakapppractice.presentation.nav.setupNavGraph
 import com.sonchan.dobakapppractice.ui.theme.DobakAppPracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -50,18 +28,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             DobakAppPracticeTheme {
                 val navController = rememberNavController()
-                setupNavGraph(navController = navController, "login")
+                NavigationGraph(navController = navController, "login")
             }
-//            DobakAppPracticeTheme {
-//                val navController = rememberNavController()
-//                Scaffold(
-//                    bottomBar = { BottomNavigation(navController = navController) }
-//                ) {
-//                    Box(Modifier.padding(it)) {
-//                        NavigationGraph(navController = navController)
-//                    }
-//                }
-//            }
         }
     }
 }
@@ -69,11 +37,15 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen() {
     val navController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
     Scaffold(
-        bottomBar = { BottomNavigation(navController = navController) }
+        bottomBar = {  if (currentRoute != "login" && currentRoute != "profile") {
+            BottomNavigation(navController)
+        }}
     ) {
         Box(Modifier.padding(it)) {
-            NavigationGraph(navController = navController)
+            NavigationGraph(navController = navController, BottomNavItem.Dobak.screenRoute)
         }
     }
 }
