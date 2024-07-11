68124837896aaa392896c971b0c19b9d5d8f0d36
손찬형 <sonchanhyeong710@gmail.com>
2024-07-11 12:10:25 +0900

    안쓰는 Import 삭제

--- app/src/main/java/com/sonchan/dobakapppractice/presentation/nav/BottomNavigation.kt
+++ app/src/main/java/com/sonchan/dobakapppractice/presentation/nav/BottomNavigation.kt
@@ -1,6 +1,5 @@
 package com.sonchan.dobakapppractice.presentation.nav
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -8,7 +7,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,25 +14,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
-
-@Preview
-@Composable
-fun BottomNavigationPreview() {
-    val navController = rememberNavController()
-    Scaffold(
-        bottomBar = { BottomNavigation(navController = navController) }
-    ) {
-        Box(Modifier.padding(it)) {
-            NavigationGraph(navController = navController)
-        }
-    }
-}
 
 @Composable
 fun BottomNavigation(navController: NavHostController) {
