77d2f7794309d2c8f67371d80a54ba2c99625be6
손찬형 <sonchanhyeong710@gmail.com>
2024-07-11 12:09:57 +0900

    SetupNavGraph 내용 이동, 수정

--- app/src/main/java/com/sonchan/dobakapppractice/presentation/nav/NavigationGraph.kt
+++ app/src/main/java/com/sonchan/dobakapppractice/presentation/nav/NavigationGraph.kt
@@ -1,25 +1,37 @@
 package com.sonchan.dobakapppractice.presentation.nav
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.sonchan.dobakapppractice.presentation.login.GoogleAuthUiClientProvider.googleAuthUiClient
+import com.sonchan.dobakapppractice.presentation.login.LoginScreen
+import com.sonchan.dobakapppractice.presentation.login.LoginViewModel
 import com.sonchan.dobakapppractice.presentation.main.DobakScreen
-import com.sonchan.dobakapppractice.presentation.main.MainScreen
 import com.sonchan.dobakapppractice.presentation.main.DobakViewModel
+import com.sonchan.dobakapppractice.presentation.main.MainScreen
 import com.sonchan.dobakapppractice.presentation.mine.MineScreen
 import com.sonchan.dobakapppractice.presentation.mine.MineViewModel
+import com.sonchan.dobakapppractice.presentation.profile.ProfileScreen
 import com.sonchan.dobakapppractice.presentation.rank.RankScreen
+import kotlinx.coroutines.launch
 
 @Composable
-fun NavigationGraph(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = BottomNavItem.Dobak.screenRoute) {
+fun NavigationGraph(navController: NavHostController, destination: String) {
+    NavHost(navController = navController, startDestination = destination) {
         composable(BottomNavItem.Dobak.screenRoute) {
             DobakScreen(
                 viewModel = DobakViewModel
@@ -35,15 +47,91 @@ fun NavigationGraph(navController: NavHostController) {
         composable(BottomNavItem.Rank.screenRoute) {
             RankScreen()
         }
-        composable("profile") {
-            val navController = rememberNavController()
-            Scaffold(
-                bottomBar = {  }
-            ) {
-                Box(Modifier.padding(it)) {
-                    setupNavGraph(navController = navController, "profile")
+        composable("login") {
+            val viewModel = viewModel<LoginViewModel>()
+            val state by viewModel.state.collectAsStateWithLifecycle()
+            val coroutineScope = rememberCoroutineScope()
+            val context = LocalContext.current
+
+            LaunchedEffect(key1 = Unit) {
+                if (googleAuthUiClient.getSignedInUser() != null) {
+                    navController.navigate("main")
                 }
             }
+
+            val launcher = rememberLauncherForActivityResult(
+                contract = ActivityResultContracts.StartIntentSenderForResult(),
+                onResult = { result ->
+                    if (result.resultCode == ComponentActivity.RESULT_OK) {
+                        coroutineScope.launch {
+                            val signInResult = googleAuthUiClient.signInWithIntent(
+                                intent = result.data ?: return@launch
+                            )
+                            viewModel.onSignInResult(signInResult)
+                        }
+                    }
+                }
+            )
+
+            LaunchedEffect(key1 = state.isSignInSuccessful) {
+                if (state.isSignInSuccessful) {
+                    Toast.makeText(
+                        context,
+                        "성공",
+                        Toast.LENGTH_LONG
+                    ).show()
+
+                    navController.navigate("main"){
+                        popUpTo(navController.graph.id){
+                            inclusive = true
+                        }
+                    }
+                    viewModel.resetState()
+                }
+            }
+
+            LoginScreen(
+                state = state,
+                onSignInClick = {
+                    coroutineScope.launch {
+                        val signInIntentSender = googleAuthUiClient.signIn()
+                        launcher.launch(
+                            IntentSenderRequest.Builder(
+                                signInIntentSender ?: return@launch
+                            ).build()
+                        )
+                    }
+                }
+            )
+        }
+        composable("profile") {
+            val coroutineScope = rememberCoroutineScope()
+            val context = LocalContext.current
+
+            ProfileScreen(
+                userData = googleAuthUiClient.getSignedInUser(),
+                onSignOut = {
+                    coroutineScope.launch {
+                        googleAuthUiClient.signOut()
+                        Toast.makeText(
+                            context,
+                            "로그아웃됨",
+                            Toast.LENGTH_LONG
+                        ).show()
+
+                        navController.navigate("login"){
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                inclusive = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    }
+                }
+            )
+        }
+        composable("main") {
+            MainScreen()
         }
     }
 }
