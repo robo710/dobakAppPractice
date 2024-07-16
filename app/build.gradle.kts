d1c66935b132ee048c13c6ce00874604288bc4e7
손찬형 <sonchanhyeong710@gmail.com>
2024-07-16 17:52:20 +0900

    gradle 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,7 +79,7 @@ dependencies {
     // TODO: Add the dependencies for Firebase products you want to use
     // When using the BoM, don't specify versions in Firebase dependencies
     implementation("com.google.firebase:firebase-database")
-    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-auth:21.0.8")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
 
     // navigation
